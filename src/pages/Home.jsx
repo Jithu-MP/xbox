2f295ad816839087f00fd3fe215024489693aa44
lakshmianirudh <lakshmi.u@keyvalue.systems>
2023-09-15 15:10:58 +0530

    feat : nav bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import Button from '@mui/material/Button';
 import PinDropIcon from '@mui/icons-material/PinDrop';
+import ButtonAppBar from "../components/navbar/Navbar";
 
 const HomePage = () => {
   return (
     <div style={{ background: "red", height: "100vh" }}>
-      Home page{" "}
-      <Button variant="contained" endIcon={<PinDropIcon />}>
+      <ButtonAppBar />
+      {/* <Button variant="contained" endIcon={<PinDropIcon />}>
         Send
-      </Button>
+      </Button> */}
     </div>
   );
 };
